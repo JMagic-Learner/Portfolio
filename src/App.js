f39b37719247f95d7314bf0e4e3808285b25328e
JasonDesktop <MaJason93@gmail.com>
2022-07-19 23:53:30 -0700

    Testing Heroku, default create-react-app

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ import About from './components/About';
 import Services from './components/Services';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-             <Navigation/>
-        {/* <Introduction/>
-        <About/>
-        <Services/>
-        <Projects/>
-        <Contact/>
-        <Footer/> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
